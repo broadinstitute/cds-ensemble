75081be21e6d002bf6777143e111e1ed4bfbb677
Yejia Chen <yejia@broadinstitute.org>
2020-10-16 09:18:22 -0400

    Oops actually fix typo?

--- cds_ensemble/run_ensemble.py
+++ cds_ensemble/run_ensemble.py
@@ -234,7 +234,7 @@ class EnsembleRegressor:
             raise xerror
         for df in X[1:]:
             if not all(df.index == X[0].index):
-                raise x
+                raise xerror
 
     def fit(self, X, Y, columns=None, report_freq=20):
         """
