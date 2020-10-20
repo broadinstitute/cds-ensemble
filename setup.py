95b207f4035382f3367a5e4e9f5a10811c8d361f
Yejia Chen <yejia@broadinstitute.org>
2020-10-20 11:12:43 -0400

    Add typing-extensions to requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ setup(
     name="cds-ensemble",
     version="0.1",
     py_modules=[],
-    install_requires=["click>=7", "pandas>=1", "pyyaml>=5", "scikit-learn>=0"],
+    install_requires=[
+        "click>=7",
+        "pandas>=1",
+        "pyyaml>=5",
+        "scikit-learn>=0",
+        "typing-extensions>=3",
+    ],
     entry_points="""
         [console_scripts]
         cds-ensemble=cds_ensemble.__main__:main
