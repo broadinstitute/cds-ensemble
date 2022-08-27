4d753a6baa1393103a24cc066d3a6cdd2e682b16
J. Michael Krill-Burger <30512586+jmkrillburger@users.noreply.github.com>
2022-08-26 23:42:32 -0400

    Updated package requirements

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="cds-ensemble",
-    version="0.1",
+    version="0.2",
     packages=find_packages(include=["cds_ensemble"]),
     install_requires=[
         "click>=7,<8",
-        "pandas>=1,<2",
-        "pyarrow>=3,<4",
-        "pyyaml>=5,<6",
-        "scikit-learn>=0,<1",
-        "typing-extensions>=3,<4",
+        "numpy==1.21.6",
+        "pandas==1.3.5",
+        "pyarrow==9.0.0",
+        "scipy==1.7.3",
+        "scikit-learn==1.0.2",
     ],
     entry_points="""
         [console_scripts]
